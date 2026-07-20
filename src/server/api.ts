288dacb7504aab6a8339c318cf1321d7ec0337b6
Agaba Arnold <agabaarnold23@gmail.com>
2026-07-20 20:52:46 +0300

    feat: implement custom error handling with TmdbApiError class in API client

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,6 +1,24 @@
 import axios from "axios";
 import { env } from "#/env/backend";
 
+export class TmdbApiError extends Error {
+    status: number | undefined;
+    code: number | undefined;
+
+    constructor(
+        message: string,
+        {
+            status,
+            code,
+        }: { status: number | undefined; code: number | undefined }
+    ) {
+        super(message);
+        this.name = "TmdbApiError";
+        this.status = status;
+        this.code = code;
+    }
+}
+
 const tmdbClient = axios.create({
     baseURL: env.TMDB_BASE_URL,
     headers: { Accept: "application/json" },
@@ -18,10 +36,26 @@ tmdbClient.interceptors.request.use((config) => {
 
 tmdbClient.interceptors.response.use(
     (res) => res,
-    (error) => {
+    (error: unknown) => {
+        if (!axios.isAxiosError(error)) {
+            return Promise.reject(error);
+        }
+
+        const data = error.response?.data as
+            | { status_message?: string; status_code?: number }
+            | undefined;
+
         const message =
-            error.response?.data?.message ?? "Something went wrong. Try again";
-        return Promise.reject(new Error(message));
+            data?.status_message ??
+            error.message ??
+            "Something went wrong. Try again";
+
+        return Promise.reject(
+            new TmdbApiError(message, {
+                code: data?.status_code,
+                status: error.response?.status,
+            })
+        );
     }
 );
 
