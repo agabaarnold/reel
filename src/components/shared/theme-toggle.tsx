e08a3fd6b8e1e3cae18f665ea182b1d0ea441305
Agaba Arnold <agabaarnold23@gmail.com>
2026-07-20 18:29:56 +0300

    feat: refactor ThemeToggle component to use useCallback for theme setting

--- src/components/shared/theme-toggle.tsx
+++ src/components/shared/theme-toggle.tsx
@@ -1,9 +1,9 @@
 import { IconDeviceDesktop, IconMoon, IconSun } from "@tabler/icons-react";
+import { useCallback } from "react";
 import { Button } from "../ui/button";
 import {
     DropdownMenu,
     DropdownMenuItem,
-    DropdownMenuSubContent,
     DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
 import { useTheme } from "./theme-provider";
@@ -11,31 +11,31 @@ import { useTheme } from "./theme-provider";
 export function ThemeToggle() {
     const { setTheme } = useTheme();
 
+    const setLight = useCallback(() => setTheme("light"), [setTheme]);
+    const setDark = useCallback(() => setTheme("dark"), [setTheme]);
+    const setSystem = useCallback(() => setTheme("system"), [setTheme]);
+
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger>
-                <Button size="icon" variant="outline">
-                    <IconSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <DropdownMenuTrigger>
+            <Button size="icon" variant="outline">
+                <IconSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <IconMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+            </Button>
 
-                    <IconMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                    
-                    <span className="sr-only">Toggle theme</span>
-                </Button>
-            </DropdownMenuTrigger>
-
-            <DropdownMenuSubContent>
-                <DropdownMenuItem onClick={() => setTheme("light")}>
+            <DropdownMenu>
+                <DropdownMenuItem onClick={setLight}>
                     <IconSun className="mr-2 h-4 w-4" /> Light
                 </DropdownMenuItem>
 
-                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                <DropdownMenuItem onClick={setDark}>
                     <IconMoon className="mr-2 h-4 w-4" /> Dark
                 </DropdownMenuItem>
-
-                <DropdownMenuItem onClick={() => setTheme("system")}>
+                
+                <DropdownMenuItem onClick={setSystem}>
                     <IconDeviceDesktop className="mr-2 h-4 w-4" /> System
                 </DropdownMenuItem>
-            </DropdownMenuSubContent>
-        </DropdownMenu>
+            </DropdownMenu>
+        </DropdownMenuTrigger>
     );
 }
